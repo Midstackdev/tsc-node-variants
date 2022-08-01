8bbc960c8340610160cddcd2bde978a1409b9e52
Midstackdev <midstackdev@gmail.com>
2022-08-01 14:04:08 +0000

    add validaton

--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controllers/Author';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/', controller.createAuthor);
+router.post('/', ValidateSchema(Schemas.author.create), controller.createAuthor);
 router.get('/', controller.readAll);
 router.get('/:id', controller.readAuthor);
-router.patch('/:id', controller.updateAuthor);
+router.patch('/:id', ValidateSchema(Schemas.author.update), controller.updateAuthor);
 router.delete('/:id', controller.deleteAuthor);
 
 export default router;
--- src/routes/Book.ts
+++ src/routes/Book.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import controller from '../controllers/Book';
+import { Schemas, ValidateSchema } from '../middleware/ValidateSchema';
 
 const router = express.Router();
 
-router.post('/', controller.createBook);
+router.post('/', ValidateSchema(Schemas.book.create), controller.createBook);
 router.get('/', controller.readAll);
 router.get('/:id', controller.readBook);
-router.patch('/:id', controller.updateBook);
+router.patch('/:id', ValidateSchema(Schemas.book.update), controller.updateBook);
 router.delete('/:id', controller.deleteBook);
 
 export default router;
